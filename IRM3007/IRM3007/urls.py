dacce42ce7fd07e3cc370f5a0c3e972416f5610a
nick fava <nicolasfava@cmail.carleton.ca>
2026-03-16 16:50:02 -0400

    Finally deloyable! doesnt do much but HTML is connected

--- IRM3007/IRM3007/urls.py
+++ IRM3007/IRM3007/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.dashboard, name='home'),  # <-- root URL
+    path('dashboard/', views.dashboard, name='dashboard'),  # optional
 ]
--- IRM3007/grades/templates/urls.py
+++ IRM3007/grades/templates/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('IRM3007.urls')),
+    path('', include('IRM3007.urls')),  # Make sure this points to your app
 ]
