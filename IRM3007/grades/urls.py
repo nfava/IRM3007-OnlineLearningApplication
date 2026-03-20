6db4f9feace65aa309c74d1f8859582e93754e96
nick fava <nicolasfava@cmail.carleton.ca>
2026-03-20 16:14:20 -0400

    fixing file formatting and creating tables

--- IRM3007/grades/urls.py
+++ IRM3007/grades/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('IRM3007.urls')),
+    path('', views.dashboard, name='dashboard'),
 ]
