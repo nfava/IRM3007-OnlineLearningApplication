92e4dd6a655f272813c1d13a9a486d3dc6084b77
nick fava <nicolasfava@cmail.carleton.ca>
2026-03-21 10:08:11 -0400

    Trying to allow students to submit work, table wont create

--- IRM3007/grades/models.py
+++ IRM3007/grades/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+# Assignment model (set by admin) to ensure submission is before date
+class Assignment(models.Model):
+    title = models.CharField(max_length=200)
+    due_date = models.DateTimeField()
+    description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.title
+
+# Submission model (uploaded by student)
+class Submission(models.Model):
+    student_name = models.CharField(max_length=100)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='submissions/')
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def is_late(self):
+        return self.submitted_at > self.assignment.due_date
--- IRM3007/grades/urls.py
+++ IRM3007/grades/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('grades/', views.grades, name='grades'),
     path('converter/', views.gpa, name='converter'),
+    path('submit/', views.submit_assignment, name='submit')
 ]
