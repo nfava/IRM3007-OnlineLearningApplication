f46cf4e10837717b2a1c5a8f4f8c5df54ff0c56e
nick fava <nicolasfava@cmail.carleton.ca>
2026-03-21 07:51:22 -0400

    chaning file names as asked by prof

--- IRM3007/settingsandmigrations/asgi.py
+++ IRM3007/settingsandmigrations/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for IRM3007 project.
+ASGI config for settingsandmigrations project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IRM3007.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settingsandmigrations.settings')
 
 application = get_asgi_application()
--- IRM3007/settingsandmigrations/wsgi.py
+++ IRM3007/settingsandmigrations/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for IRM3007 project.
+WSGI config for settingsandmigrations project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IRM3007.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settingsandmigrations.settings')
 
 application = get_wsgi_application()
