199e829fd7b1db82c583c41219062c2ea086c3d0
nick fava <nicolasfava@cmail.carleton.ca>
2026-03-21 08:17:55 -0400

    I make buttons on home page! and start grades.html work

--- IRM3007/grades/views.py
+++ IRM3007/grades/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import GradeProcess
 
 def dashboard(request):
-    grades = GradeProcess.objects.all()
-    return render(request, 'dashboard.html', {'grades': grades})
+    return render(request, 'dashboard.html')
 
-def gpa_converter_view(request):
-    return render(request, 'gpa_converter.html', {})
+def grades(request):
+    return render(request, 'grades.html')
+
+def gpa(request):
+    return render(request, 'gpa_converter.html')
