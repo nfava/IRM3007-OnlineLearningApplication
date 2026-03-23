69dfdf1e1e093e2ce9bc49d73049a24611cd9354
BogdanStanley <bogdanstanley@cmail.carleton.ca>
2026-03-23 16:08:49 -0400

    Fixed converter display error

--- IRM3007/grades/urls.py
+++ IRM3007/grades/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('grades/', views.grades, name='grades'),
-    path('converter/', views.gpa, name='converter'),
+    path('converter/', views.gpa_converter_view, name='converter'),
     path('submit/', views.submit_assignment, name='submit')
 ]
