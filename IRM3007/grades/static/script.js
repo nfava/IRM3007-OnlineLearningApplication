7dc6731fa01380a979e136c6722c64e994c83492
BogdanStanley <bogdanstanley@cmail.carleton.ca>
2026-03-24 10:54:40 -0400

    Moved JS to script.js

--- IRM3007/grades/static/script.js
+++ IRM3007/grades/static/script.js
@@ -15,3 +15,26 @@ window.onclick = function(event) {
     }
   }
 }
+
+// Update converter based on selected field
+function updateFields() {
+  var conversionType = document.getElementById("id_conversion_type").value;
+  var percentageField = document.getElementById("percentage-field");
+  var gpa12Field = document.getElementById("gpa12-field");
+  if (conversionType === "percent_to_all") {
+    percentageField.style.display = "block";
+    gpa12Field.style.display = "none";
+    document.getElementById("id_gpa_12").value = "";
+  }
+  else if (conversionType === "gpa12_to_gpa4") {
+    percentageField.style.display = "none";
+    gpa12Field.style.display = "block";
+    document.getElementById("id_percentage").value = "";
+  }
+}
+// Run on page load
+document.addEventListener("DOMContentLoaded", function () {
+  updateFields();
+  // Run whenever dropdown changes
+  document.getElementById("id_conversion_type").addEventListener("change", updateFields);
+});
