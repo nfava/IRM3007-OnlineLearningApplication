a50801d5d6c3a79f96798f30f80d56cbb106fee5
BogdanStanley <bogdanstanley@cmail.carleton.ca>
2026-03-28 11:24:02 -0400

    Removed remnants of old GPA converter

--- IRM3007/grades/views.py
+++ IRM3007/grades/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from .forms import SubmissionForm, GPAConverterForm, GradeSubmissionForm, AssignmentForm
+from .forms import SubmissionForm, GradeSubmissionForm, AssignmentForm
 from .models import Assignment, Submission
 
 def dashboard(request):
